f8fa6f3a8e2bab1935ca16dd29d52ee4ac64c34f
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-12-10 15:46:44 +0200

    implementing nav bar design

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adopt Don't Shop!</title>
+    <link rel="stylesheet" href="assets/design/landing.css"/>
+</head>
+<body>
+    <main>
+        <div class="big-wrapper">
+            <header>
+                <div class="container">
+                    <div class="logo">
+                        <img src="{{url('/assets/img/logo.png')}}"/>
+
+                    </div>
+
+                    <div class="links">
+                        <ul>
+                            <li><a href="#">Features</a></li>
+                            <li><a href="#">Mission</a></li>
+                            <li><a href="#">Volunteer</a></li>
+                            <li><a href="#" class="btn">Sign In</a></li>
+                    </ul>
+                    </div>
+                </div>
+            </header>
+        </div>
+
+    </main>
+</body>
+</html>
