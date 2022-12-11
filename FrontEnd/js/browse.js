d1075c34accb1cf98205180a9d9cd19602ef9d4c
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-12-11 18:51:41 +0200

    Finishing browsing page

--- FrontEnd/js/browse.js
+++ FrontEnd/js/browse.js
@@ -3,3 +3,7 @@ const hamburger = document.querySelector(".hamburger");
 hamburger.addEventListener('click', function(){
     this.classList.toggle('is-active');
 })
+
+function seeDetails(){
+    window.location.href ="dog_profile.html";
+}
