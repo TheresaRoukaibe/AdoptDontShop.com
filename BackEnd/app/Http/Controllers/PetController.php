b77e999e0d8fe7b2b64f332d8731f4e3fb0a7915
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-12-12 21:33:41 +0200

    Implementing get dog API

--- BackEnd/app/Http/Controllers/PetController.php
+++ BackEnd/app/Http/Controllers/PetController.php
@@ -3,10 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Pet;
 
 class PetController extends Controller
 {
-    function get_dogs(){
-        
+    function get_dog($id = null){
+        if($id){
+            $dog=Pet::find($id);
+            if($dog){
+            return response()->json([
+                "status" => $dog
+            ]);
+        }else{
+            return response()->json([
+                "status" => "No dogs in db"
+            ]);
+        }
+            
+        }else{
+           $dogs = Pet::where("isAdopted", "=", 0);
+           return response()->json([
+            "status" => $dogs
+        ]);
+        }
+
     }
 }
