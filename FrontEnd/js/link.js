2c75b888ba428de5045c19115c1dbeba21819a0e
TheresaRoukaibe <theresaroukaibe@gmail.com>
2022-12-15 19:05:37 +0200

    Inserting in HTML

--- FrontEnd/js/link.js
+++ FrontEnd/js/link.js
@@ -166,7 +166,7 @@ adoption_pages.load_applicants = async () =>{
     const response_apps = await adoption_pages.getAPI(get_apps_url);
     const apps_data = response_apps.data;
     const division = document.getElementById("center");
-    
+    division.innerHTML += "<div class='profile'>  <div class='image'> <img src='C:\Users\User\Desktop\adoptDontShop\FrontEnd\assets\img\profile.svg' width='70' height='70'></div><div class='name'>" +apps_data.status[i].name +"</div><div class='actions'><button class='btn'>Choose</button></div></div>";
 }
 adoption_pages.load_adopted = async () => {
     const user_id = window.localStorage.getItem('id');
